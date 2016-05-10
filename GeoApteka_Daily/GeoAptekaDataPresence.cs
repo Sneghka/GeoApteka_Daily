8979214ed8a48f0bb199403ce3b3b8501a20b8a8
Sneghka <sneghkan@mail.ru>
2016-05-10 09:43:55 +0300

    Check one day before today. You need enter login&password.

---
+++ GeoApteka_Daily/GeoAptekaDataPresence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using GeoApteka_Daily.WebPageElements;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenQA.Selenium.Support.Extensions;
+
+
+
+namespace GeoApteka_Daily
+{
+    [TestFixture]
+    public class GeoAptekaDataPresence
+    {
+        [Test]
+        public void IsDataPresent()
+        {
+            var firefox = new FirefoxDriver();
+            firefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(60));
+            var pageElements = new PageElements(firefox);
+
+            firefox.Navigate().GoToUrl("http://pharmxplorer.com.ua/572");
+
+            var loginElement = pageElements.LoginElement;
+            var passwordElement = pageElements.PasswordElement;
+            var loginButton = pageElements.LoginButton;
+            loginElement.SendKeys("");                      //ENTER LOGIN
+            passwordElement.SendKeys("");                   //ENTER PASSWORD
+            loginButton.Click();
+
+            firefox.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10));
+
+            var dateDropDownButton = pageElements.DateDropDownButton;
+            dateDropDownButton.Click();
+            System.Threading.Thread.Sleep(7000);
+
+            var lastDate = pageElements.LastDate;
+            lastDate.Click();
+            System.Threading.Thread.Sleep(5000);
+            var choosenDate = pageElements.ChoosenDate;
+            //System.Threading.Thread.Sleep(7000);
+            //Assert.AreEqual("27.04.2016", choosenDate.Text);
+
+            System.Threading.Thread.Sleep(5000);
+
+            ITakesScreenshot screenshotDriver = firefox as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            String fp = "D:\\" + "snapshot" + ".png";
+            screenshot.SaveAsFile(fp, ImageFormat.Png);
+            
+
+            pageElements.email_send();
+
+
+            firefox.Quit();
+
+        }
+    }
+}
---
+++ GeoApteka_Daily/WebPageElements/PageElements.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace GeoApteka_Daily.WebPageElements
+{
+    public class PageElements
+    {
+        private readonly FirefoxDriver _firefox;
+
+        public PageElements(FirefoxDriver firefox)
+        {
+            _firefox = firefox;
+
+        }
+
+        public IWebElement LoginElement
+        {
+            get { return _firefox.FindElement(By.Id("username")); }
+        }
+        public IWebElement PasswordElement
+        {
+            get { return _firefox.FindElement(By.Id("password")); }
+        }
+
+        public IWebElement LoginButton
+        {
+            get { return _firefox.FindElement(By.Id("submit")); }
+        }
+
+        public IWebElement DateDropDownButton
+        {
+            get { return _firefox.FindElement(By.CssSelector("img.Qv_CellIcon_right")); }
+        }
+        public IWebElement LastDate
+        {
+            get { return _firefox.FindElement(By.XPath(".//*[@id='DS']/div/div/div[1]/div[1]/div[1]")); }
+        }
+
+        public IWebElement ChoosenDate
+        {
+            get { return _firefox.FindElement(By.CssSelector(".QvSliderObject>table>tbody>tr>td")); }
+        }
+
+        public void email_send()
+        {
+            var mail = new MailMessage();
+            var smtpServer = new SmtpClient("post.morion.ua");
+            mail.From = new MailAddress("snizhana.nomirovska@proximaresearch.com");
+            mail.To.Add("snizhana.nomirovska@proximaresearch.com");
+           // mail.To.Add("nataly.tenkova@proximaresearch.com");
+            mail.Subject = "Test Mail - 1";
+            mail.Body = "mail with attachment";
+
+            var attachment = new Attachment("d:/snapshot.png");
+            mail.Attachments.Add(attachment);
+
+
+            //smtpServer.Port = 110;
+            //smtpServer.Credentials = new NetworkCredential("snizhana.nomirovska@proximaresearch.com", "s.nomirovska2016");
+            //smtpServer.EnableSsl = true;
+
+            smtpServer.Send(mail);
+
+        }
+
+    }
+}
